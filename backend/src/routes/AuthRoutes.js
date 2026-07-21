6e674c54dc83ed1516cff2d9e81cfe4ee4833bac
Gabriel Guerrero <guerrerog675@gmail.com>
2026-07-21 13:16:07 -0600

    Fix verifyToken for register

--- backend/src/routes/AuthRoutes.js
+++ backend/src/routes/AuthRoutes.js
@@ -6,6 +6,7 @@ const router = Router();
 router.get("/status", getSystemStatus);
 router.get("/username/:id", getUsername);
 router.post("/login", login);
-router.post("/register", verifyToken, register);
+//router.post("/register", verifyToken, register);
+router.post("/register", register);// Para mientras en producción ya que no ha ejecutado los seeders
 router.post("/logout",verifyToken,logout);
 export default router;
