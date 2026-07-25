fa31b09a5975efda87154ac2103221dc5cf1e884
Gabriel Guerrero <guerrerog675@gmail.com>
2026-07-25 12:19:12 -0600

    fix: configure sequelize production environment

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,5 +6,10 @@ export default {
     url: process.env.DATABASE_URL,
     dialect: "postgres",
     logging: false
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    logging: false
   }
 };
